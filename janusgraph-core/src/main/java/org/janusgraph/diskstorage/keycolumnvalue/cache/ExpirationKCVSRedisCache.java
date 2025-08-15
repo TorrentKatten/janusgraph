503bf4c4ead52a7bcc54bd9fa85c4a5f3f2fc4f6
Jon-Erik Johnzon <jone@torrentkatten.se>
2025-08-15 12:50:29 +0200

    new approach to the redis-based-cache

--- janusgraph-core/src/main/java/org/janusgraph/diskstorage/keycolumnvalue/cache/ExpirationKCVSRedisCache.java
+++ janusgraph-core/src/main/java/org/janusgraph/diskstorage/keycolumnvalue/cache/ExpirationKCVSRedisCache.java
@@ -15,10 +15,7 @@
 package org.janusgraph.diskstorage.keycolumnvalue.cache;
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
-import com.google.common.cache.CacheLoader;
 import org.janusgraph.core.JanusGraphException;
 import org.janusgraph.diskstorage.BackendException;
 import org.janusgraph.diskstorage.EntryList;
@@ -29,7 +26,6 @@ import org.janusgraph.diskstorage.keycolumnvalue.KeySliceQuery;
 import org.janusgraph.diskstorage.keycolumnvalue.SliceQuery;
 import org.janusgraph.diskstorage.keycolumnvalue.StoreTransaction;
 import org.janusgraph.diskstorage.util.CacheMetricsAction;
-import org.nustaq.serialization.FSTConfiguration;
 import org.redisson.api.RLock;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
@@ -45,44 +41,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-import static org.janusgraph.util.datastructures.ByteSize.OBJECT_HEADER;
-import static org.janusgraph.util.datastructures.ByteSize.OBJECT_REFERENCE;
-import static org.janusgraph.util.datastructures.ByteSize.STATICARRAYBUFFER_RAW_SIZE;
 
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
  */
 public class ExpirationKCVSRedisCache extends KCVSCache {
 
-    //Weight estimation
-    private static final int STATIC_ARRAY_BUFFER_SIZE = STATICARRAYBUFFER_RAW_SIZE + 10; // 10 = last number is average length
-    private static final int KEY_QUERY_SIZE = OBJECT_HEADER + 4 + 1 + 3 * (OBJECT_REFERENCE + STATIC_ARRAY_BUFFER_SIZE); // object_size + int + boolean + 3 static buffers
-
-    private static final int INVALIDATE_KEY_FRACTION_PENALTY = 1000;
     private static final int PENALTY_THRESHOLD = 5;
     public static final String REDIS_CACHE_PREFIX = "redis-cache-";
     public static final String REDIS_INDEX_CACHE_PREFIX = "redis-index-cache-";
 
-    private volatile CountDownLatch penaltyCountdown;
-
-    private final ConcurrentHashMap<StaticBuffer, Long> expiredKeys;
-
     private final long cacheTimeMS;
-    private final long invalidationGracePeriodMS;
-    private final CleanupThread cleanupThread;
     private final RMapCache<KeySliceQuery, byte[]> redisCache;
     private final RMapCache<StaticBuffer, ArrayList<KeySliceQuery>> redisIndexKeys;
     private static final Logger logger = LoggerFactory.getLogger(ExpirationKCVSRedisCache.class);
     private static final ObjectSerializer serializer = new JavaSerializer();
-//    private static final ObjectSerializer serializer = new FstSerializer();
-//    private static final ObjectSerializer serializer = new JsonSerializer();
 
     public ExpirationKCVSRedisCache(final KeyColumnValueStore store, String metricsName, final long cacheTimeMS,
                                     final long invalidationGracePeriodMS, final long maximumByteSize, Configuration configuration) {
@@ -90,9 +67,8 @@ public class ExpirationKCVSRedisCache extends KCVSCache {
         Preconditions.checkArgument(cacheTimeMS > 0, "Cache expiration must be positive: %s", cacheTimeMS);
         Preconditions.checkArgument(System.currentTimeMillis() + 1000L * 3600 * 24 * 365 * 100 + cacheTimeMS > 0, "Cache expiration time too large, overflow may occur: %s", cacheTimeMS);
         this.cacheTimeMS = cacheTimeMS;
-        final int concurrencyLevel = Runtime.getRuntime().availableProcessors();
+
         Preconditions.checkArgument(invalidationGracePeriodMS >= 0, "Invalid expiration grace period: %s", invalidationGracePeriodMS);
-        this.invalidationGracePeriodMS = invalidationGracePeriodMS;
 
         RedissonClient redissonClient = RedissonCache.getRedissonClient(configuration);
         redisCache = redissonClient.getMapCache(REDIS_CACHE_PREFIX + metricsName);
@@ -101,50 +77,29 @@ public class ExpirationKCVSRedisCache extends KCVSCache {
         redisIndexKeys = redissonClient.getMapCache(REDIS_INDEX_CACHE_PREFIX + metricsName);
         redisIndexKeys.setMaxSize(75000);
 
-        expiredKeys = new ConcurrentHashMap<>(50, 0.75f, concurrencyLevel);
-        penaltyCountdown = new CountDownLatch(PENALTY_THRESHOLD);
-
-        cleanupThread = new CleanupThread();
-        cleanupThread.start();
         logger.debug("********************** Configurations are loaded **********************");
     }
 
     @Override
     public EntryList getSlice(final KeySliceQuery query, final StoreTransaction txh) throws BackendException {
-        incActionBy(1, CacheMetricsAction.RETRIEVAL, txh);
-        if (isExpired(query)) {
-            incActionBy(1, CacheMetricsAction.MISS, txh);
-            return store.getSlice(query, unwrapTx(txh));
-        }
-
         try {
-            return get(query, () -> {
+            incActionBy(1, CacheMetricsAction.RETRIEVAL, txh);
+
+            byte[] bytQuery = redisCache.get(query);
+            EntryList entries = serializer.deserialize(bytQuery);
+
+            if (entries == null) {
                 incActionBy(1, CacheMetricsAction.MISS, txh);
                 return store.getSlice(query, unwrapTx(txh));
-            });
+            } else {
+                return entries;
+            }
+
         } catch (Exception e) {
             if (e instanceof JanusGraphException) throw (JanusGraphException) e;
             else if (e.getCause() instanceof JanusGraphException) throw (JanusGraphException) e.getCause();
             else throw new JanusGraphException(e);
         }
-    }
-
-    private EntryList get(KeySliceQuery query, Callable<EntryList> valueLoader) {
-        byte[] bytQuery = redisCache.get(query);
-        EntryList entries = serializer.deserialize(bytQuery);
-        if (entries == null) {
-            try {
-                entries = valueLoader.call();
-                if (entries == null) {
-                    throw new CacheLoader.InvalidCacheLoadException("valueLoader must not return null, key=" + query);
-                } else {
-                    putToRedis(query, entries);
-                }
-            } catch (Exception e) {
-                logger.error("Failed to read from the store.", e);
-            }
-        }
-        return entries;
     }
 
     @Override
@@ -159,10 +114,15 @@ public class ExpirationKCVSRedisCache extends KCVSCache {
             final StaticBuffer key = keys.get(i);
             ksqs[i] = new KeySliceQuery(key, query);
             EntryList result = null;
-            if (!isExpired(ksqs[i])) {
-                bytResult = redisCache.get(ksqs[i]);
+
+            bytResult = redisCache.get(ksqs[i]);
+
+            if (bytResult != null) {
                 result = bytResult != null ? (EntryList) serializer.deserialize(bytResult) : null;
-            } else ksqs[i] = null;
+            } else {
+                ksqs[i] = null;
+            }
+
             if (result != null) {
                 results.put(key, result);
             } else {
@@ -230,141 +190,46 @@ public class ExpirationKCVSRedisCache extends KCVSCache {
     @Override
     public void clearCache() {
         redisCache.clearExpire();
-        expiredKeys.clear();
-        penaltyCountdown = new CountDownLatch(PENALTY_THRESHOLD);
     }
 
     public void invalidate(StaticBuffer key, List<CachableStaticBuffer> entries) {
         logger.debug("Invalidating key {}", key);
-        List<KeySliceQuery> keySliceQueryList = redisIndexKeys.get(key);
-        if (keySliceQueryList != null) {
-            List<KeySliceQuery> keySliceQueryListCopy = new ArrayList<>(keySliceQueryList);
-            for (KeySliceQuery keySliceQuery : keySliceQueryListCopy) {
-                if (key.equals(keySliceQuery.getKey())) {
-                    redisCache.remove(keySliceQuery);
+        RLock lock = redisIndexKeys.getLock(key);
+        try {
+            if (lock.tryLock(1, 2, TimeUnit.SECONDS)) {
+                List<KeySliceQuery> keySliceQueryList = redisIndexKeys.get(key);
+                if (keySliceQueryList != null) {
+                    List<KeySliceQuery> keySliceQueryListCopy = new ArrayList<>(keySliceQueryList);
+                    for (KeySliceQuery keySliceQuery : keySliceQueryListCopy) {
+                        if (key.equals(keySliceQuery.getKey())) {
+                            redisCache.remove(keySliceQuery);
+                        }
+                    }
                 }
+            } else {
+                logger.warn("Failed to acquire lock for key {}", key);
             }
-
-            Preconditions.checkArgument(!hasValidateKeysOnly() || entries.isEmpty());
-            expiredKeys.put(key, getExpirationTime());
-            if (Math.random() < 1.0 / INVALIDATE_KEY_FRACTION_PENALTY) penaltyCountdown.countDown();
+        } catch (InterruptedException e) {
+            logger.warn("Interrupted while acquiring lock from Redis", e);
+            Thread.currentThread().interrupt();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            } else {
+                logger.warn("Lock not held by current thread, skipping unlock");
+            }
         }
     }
 
     @Override
     public void close() throws BackendException {
-        cleanupThread.stopThread();
         super.close();
-    }
-
-    private boolean isExpired(final KeySliceQuery query) {
-        Long until = expiredKeys.get(query.getKey());
-        if (until == null) return false;
-        if (isBeyondExpirationTime(until)) {
-            expiredKeys.remove(query.getKey(), until);
-            return false;
-        }
-        //We suffer a cache miss, hence decrease the count down
-        penaltyCountdown.countDown();
-        return true;
-    }
-
-    private long getExpirationTime() {
-        return System.currentTimeMillis() + cacheTimeMS;
-    }
-
-    private boolean isBeyondExpirationTime(long until) {
-        return until < System.currentTimeMillis();
-    }
-
-    private class CleanupThread extends Thread {
-
-        private boolean stop = false;
-
-        public CleanupThread() {
-            this.setDaemon(true);
-            this.setName("ExpirationStoreCache-" + getId());
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                if (stop) return;
-                try {
-
-                    penaltyCountdown.await();
-                } catch (InterruptedException e) {
-                    if (stop) return;
-                    else throw new RuntimeException("Cleanup thread got interrupted", e);
-                }
-                //Do clean up work by invalidating all entries for expired keys
-                final Map<StaticBuffer, Long> expiredKeysCopy = new HashMap<>(expiredKeys.size());
-                for (Map.Entry<StaticBuffer, Long> expKey : expiredKeys.entrySet()) {
-                    if (isBeyondExpirationTime(expKey.getValue()))
-                        expiredKeys.remove(expKey.getKey(), expKey.getValue());
-                    else if (getAge(expKey.getValue()) >= invalidationGracePeriodMS)
-                        expiredKeysCopy.put(expKey.getKey(), expKey.getValue());
-                }
-                for (KeySliceQuery ksq : redisCache.keySet()) {
-                    if (expiredKeysCopy.containsKey(ksq.getKey())) {
-                        redisCache.remove(ksq);
-                    }
-                }
-                penaltyCountdown = new CountDownLatch(PENALTY_THRESHOLD);
-                for (Map.Entry<StaticBuffer, Long> expKey : expiredKeysCopy.entrySet()) {
-                    expiredKeys.remove(expKey.getKey(), expKey.getValue());
-                }
-            }
-        }
-
-        void stopThread() {
-            stop = true;
-            this.interrupt();
-        }
-
-        private long getAge(long until) {
-            long age = System.currentTimeMillis() - (until - cacheTimeMS);
-            assert age >= 0;
-            return age;
-        }
     }
 
     private interface ObjectSerializer {
         <T> byte[] serialize(T obj);
 
         <T> T deserialize(byte[] bytes);
-    }
-
-    private static class JsonSerializer implements ObjectSerializer {
-        private static final ObjectMapper objectMapper = new ObjectMapper();
-
-        @Override
-        public <T> byte[] serialize(T obj) {
-            if (obj == null) {
-                return null;
-            }
-            try {
-                return objectMapper.writeValueAsBytes(obj);
-            } catch (IOException e) {
-                logger.warn("Failed to serialize object", e);
-                throw new RuntimeException(e);
-            }
-        }
-
-        @Override
-        public <T> T deserialize(byte[] bytes) {
-            if (bytes == null) {
-                return null;
-            }
-            try {
-                return objectMapper.readValue(bytes, new TypeReference<T>() {
-                });
-            } catch (IOException e) {
-                logger.warn("Failed to deserialize object", e);
-                throw new RuntimeException(e);
-            }
-        }
-
     }
 
     private static class JavaSerializer implements ObjectSerializer {
@@ -397,20 +262,4 @@ public class ExpirationKCVSRedisCache extends KCVSCache {
             }
         }
     }
-
-    private static class FstSerializer implements ObjectSerializer {
-        private static final FSTConfiguration fastConf = FSTConfiguration.createDefaultConfiguration();
-
-        @Override
-        public <T> byte[] serialize(T obj) {
-            return fastConf.asByteArray(obj);
-        }
-
-        @Override
-        public <T> T deserialize(byte[] bytes) {
-            return (T) (bytes != null ? fastConf.asObject(bytes) : null);
-        }
-
-    }
-
 }
